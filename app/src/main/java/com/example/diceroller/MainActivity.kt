a80bbf03b2e5fc18432e6652fd55b9e9d9de7fca
weeshen0916 <weeshen0916@gmail.com>
2021-06-21 15:51:24 +0800

    update ImageView

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -15,12 +16,19 @@ class MainActivity : AppCompatActivity() {
 
 
     }
-    private fun rollDice(){
-        val randomInt = (1..6).random()
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
-        Toast.makeText(this, "button clicked",
-            Toast.LENGTH_SHORT).show()
 
+    private fun rollDice() {
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when ((1..6).random()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+
+        }
     }
-}
